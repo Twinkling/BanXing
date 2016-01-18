a4eff21c1e9c5ba2ea3afe09954feabe9c7ee02f
TwinklingZ <twinkling8023@gmail.com>
2016-01-18 16:21:41 +0800

    新增结伴旅游功能

--- libs/Controller/partnerController.class.php
+++ libs/Controller/partnerController.class.php
@@ -7,7 +7,84 @@
  */
 class partnerController{
     public function detail(){
-        print_r($_GET);
-        //VIEW::assign(array(''))
+        $sid = intval(daddslashes($_GET['sid']));
+        $sname = daddslashes($_GET['sname']);
+        $teamobj = M('partner');
+        $team = $teamobj->getAll($sid);
+        if(empty($team))
+            unset($team);
+        VIEW::assign(array('title'=>$sname."旅游队伍",'team'=>$team,'sid'=>$sid,'sname'=>$sname));
+        VIEW::display('team.html');
+    }
+    public function add(){
+        if(!isset($_SESSION['username'])){
+            $this->showmessage('请登录后操作！','index.php?controller=user&method=login');
+        }
+        if(empty($_POST['submit'])||(!isset($_POST['submit']))){
+            $sid = intval(daddslashes($_GET['sid']));
+            $sname = daddslashes($_GET['sname']);
+            $team['sid'] = $sid;
+            $team['sname'] = $sname;
+            VIEW::assign(array('title'=>"创建".$sname."旅游队伍",'team'=>$team));
+            VIEW::display('addTeam.html');
+        }else{
+            $username = array();
+            $sid = intval(daddslashes($_POST['sid']));
+            $sname = daddslashes($_POST['sname']);
+            $username[] = daddslashes($_SESSION['username']);
+            $dateline = daddslashes($_POST['date']);
+            $dateline = $this->getLeaveTime($dateline);
+            $data = array(
+                'sid'=>$sid,
+                'username'=>$username,
+                'dateline'=>$dateline
+            );
+            $teamobj = M('partner');
+            $teamobj->addTeam($data);
+            $this->showmessage("创建队伍成功！","index.php?controller=partner&method=detail&sid=".$sid."&sname=".$sname);
+        }
+    }
+    public function join(){
+        $id = intval(daddslashes($_GET['id']));
+        $sname = daddslashes($_GET['username']);
+        $teamobj = M('partner');
+        $result = $teamobj->getOne($id);
+        $username = unserialize($result['username']);
+        if(!in_array($_SESSION['username'],$username)){
+            $username[] = $_SESSION['username'];
+        }
+        $result['username'] = $username;
+        $where = 'id='.$id;
+        $teamobj->update($result,$where);
+        $this->showmessage("加入成功！",'index.php?controller=partner&method=detail&sname='.$sname.'&sid='.$result['sid']);
+    }
+
+    private function getLeaveTime($dateline){
+        $oneDay = 24*60*60;
+        switch($dateline){
+            case "one":
+                $leaveTime = time() + $oneDay;
+                break;
+            case "three":
+                $leaveTime = time() + 3*$oneDay;
+                break;
+            case "five":
+                $leaveTime = time() + 5*$oneDay;
+                break;
+            default:
+                $leaveTime = time() + $oneDay;
+                break;
+        }
+        return $leaveTime;
+    }
+
+    /**
+     * @param $info
+     * @param $url
+     * 显示特定信息，并跳转到指定页面
+     */
+    private function showmessage($info, $url){
+        echo "<script>alert('$info');window.location.href='$url'</script>";
+        exit;
     }
 }
--- libs/Model/partnerModel.class.php
+++ libs/Model/partnerModel.class.php
@@ -5,3 +5,37 @@
  * Date: 2016/1/18
  * Time: 9:06
  */
+class partnerModel{
+    private $_table = 'partner';
+
+    function getAll($sid){
+        $sql = 'select * from '.$this->_table." where sid=".$sid.' order by dateline desc';
+        $temp = DB::findAll($sql);
+        if(is_array($temp)){
+            foreach($temp as $key=>$val){
+                $username = unserialize($val['username']);
+                if(is_array($username))
+                    $val['username'] = implode(",",$username);
+                $val['dateline'] = date("Y-m-d  T",$val['dateline']);
+                $temp[$key] = $val;
+            }
+        }elseif(!empty($temp)){
+            $temp['username'] = unserialize($temp['username']);
+            $temp['dateline'] = date("Y-m-d  T",$temp['dateline']);
+        }
+        return $temp;
+    }
+
+    function getOne($id){
+        $sql = 'select * from '.$this->_table.' where id='.$id;
+        return DB::findOne($sql);
+    }
+    function update($data,$where){
+        $data['username'] = serialize($data['username']);
+        return DB::update($this->_table,$data,$where);
+    }
+    function addTeam($data){
+        $data['username'] = serialize($data['username']);
+        return DB::insert($this->_table,$data);
+    }
+}
